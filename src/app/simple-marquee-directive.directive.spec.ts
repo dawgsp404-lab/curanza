356b1a5f0e4b50f4251805064c26cd219c79b1ef
Akash <19772@geojittechnologies.com>
2025-10-21 16:09:00 +0530

    no message

---
+++ src/app/simple-marquee-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SimpleMarqueeDirectiveDirective } from './simple-marquee-directive.directive';
+
+describe('SimpleMarqueeDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SimpleMarqueeDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
