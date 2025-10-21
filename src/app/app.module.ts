f71f63d0bb6f9bd809e43e1d4a6021928826ed03
Akash <19772@geojittechnologies.com>
2025-10-21 16:08:16 +0530

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SimpleMarqueeDirectiveDirective } from './simple-marquee-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleMarqueeDirectiveDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
